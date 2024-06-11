8deea172dbf744c4604a536d21cd0eef3b697871
khaimp <khaimp@gmail.com>
2024-06-11 14:20:10 +0700

    update ENV

--- react/features/chat/ENV.ts
+++ react/features/chat/ENV.ts
@@ -1,6 +1,10 @@
 export const CMEET_ENV = {
      avatar: "files.cmcati.vn/ftp/a4b3bd7f-3763-4d7b-a2a3-9518ea0b1b9f.png",
-     urlWS: "https://sec.cmcati.vn/websocket-0.0.1/ws",
+     urlWS: "https://sec.cmcati.vn/qh-server-socket/ws",
      public: "/app/chat.send",
-     subrice: "/topic/chat"
+     subrice: "/topic/chat",
+
+     //caption
+     publicCaption: "/app/caption",
+     subriceCaption: "/topic/caption",
 }
