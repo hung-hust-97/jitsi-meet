5623b137339fac7c9bebb4fc647a4b132ebe04fe
long <doduclong08102001@gmail.com>
2024-06-17 17:18:22 +0700

    update: name display

--- react/features/display-name/components/native/DisplayNameLabel.tsx
+++ react/features/display-name/components/native/DisplayNameLabel.tsx
@@ -50,7 +50,8 @@ class DisplayNameLabel extends React.Component<IProps> {
         return (
             <View style={(this.props.contained ? styles.displayNamePadding : styles.displayNameBackdrop) as ViewStyle}>
                 <Text numberOfLines={1} style={styles.displayNameText as TextStyle}>
-                    {/* { this.props._participantName } */ "tuấn"}
+                    
+                    {/* { this.props._participantName } */ "A Hai avbc  "}
                 </Text>
             </View>
         );
