7b815196d6d1258d3805ceeec1a1d73db7f73a8d
Ninh2k1PTIT <nguyentienhaininh@gmail.com>
2024-08-08 16:01:06 +0700

    fix(spell)

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ var config = {
         domain: 'jitsi-meet.example.com',
 
         // When using authentication, domain for guest users.
-        anonymousdomain: 'guest.jitmeet.example.com',
+        anonymousdomain: 'guest.jitsi-meet.example.com',
 
         // Domain for authenticated users. Defaults to <domain>.
         // authdomain: 'jitsi-meet.example.com',
